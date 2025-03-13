20924123d93f54200cd623289670638959a59e99
Sthembiso Radebe <stheradebe@Sthembisos-MacBook-Pro.local>
2025-03-13 22:20:24 +0200

    Controllers and routes set up for the server

--- server.js
+++ server.js
@@ -2,19 +2,9 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 3004;
+const port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.status().send("Hello World!");
-});
-
-app.get("/food", (req, res) => {
-  res.send("Hello World! Food is here");
-});
-
-app.get("/users", (req, res) => {
-  res.send("The users will go here");
-});
+app.use("/api/contacts", require("./routes/contactRoutes"));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
