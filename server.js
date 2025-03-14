fb25df0e40ac1904976155a7b5dae0c63d08ae7a
Sthembiso Radebe <stheradebe@Sthembisos-MacBook-Pro.local>
2025-03-14 20:38:12 +0200

    Middleware added

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const errorHandler = require("./middleware/errorHandler");
 const dotenv = require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+//Whenever we need to use Middleware, we can make use of the app.use() method
+app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoutes"));
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
