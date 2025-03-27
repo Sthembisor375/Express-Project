f1f60a40d96888d01a7c6b4a1c45325894d8e4b1
Sthembiso Radebe <stheradebe@Sthembisos-MacBook-Pro.local>
2025-03-27 16:26:02 +0200

    errorHandler middleware updated and Express async handler added

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,14 +1,16 @@
+const asyncHandler = require("express-async-handler");
+
 //@desc Get all contacts
 //@routr GET /api/contacts
 //@access public
-const getContacts = (req, res) => {
+const getContacts = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Get all contacts" });
-};
+});
 
 //@desc Create new contact
 //@routr POST /api/contacts
 //@access public
-const createContact = (req, res) => {
+const createContact = asyncHandler(async (req, res) => {
   console.log("The request body is:", req.body);
   const { name, email, phone } = req.body;
   if (!name || !email || !phone) {
@@ -16,28 +18,28 @@ const createContact = (req, res) => {
     throw new Error("All fields are mandatory");
   }
   res.status(201).json({ message: `Update contact ${req.params.id}` });
-};
+});
 
 //@desc Get contact
 //@routr GET /api/contacts/:id
 //@access public
-const getContact = (req, res) => {
+const getContact = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Get contact for ${req.params.id}` });
-};
+});
 
 //@desc Update contact
 //@routr PUT /api/contacts/:id
 //@access public
-const updateContact = (req, res) => {
+const updateContact = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update contact for ${req.params.id}` });
-};
+});
 
 //@desc Delete contacts
 //@routr DELETE /api/contacts/:id
 //@access public
-const deleteContact = (req, res) => {
+const deleteContact = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete contact ${req.params.id}` });
-};
+});
 
 module.exports = {
   getContacts,
--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,6 +1,49 @@
+const { constants } = require("../constants");
+
 const errorHandler = (err, req, res, next) => {
   const statusCode = res.statusCode ? res.statusCode : 500;
-  res.json({ message: err.message, stackTrace: err.stack });
+  switch (statusCode) {
+    case constants.VALIDATION_ERROR:
+      res.json({
+        title: "Validation Failed",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+    case constants.NOT_FOUND:
+      res.join({
+        title: "Not Found",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+    case constants.UNAUTHORIZED:
+      res.join({
+        title: "Unauthorized",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+    case constants.FORBIDDEN:
+      res.join({
+        title: "Forbidden",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+    case constants.SERVER_ERROR:
+      res.join({
+        title: "Server error",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+    default:
+      console.log("No error, all good!");
+      break;
+  }
+  res.json({ title: "Not Found", message: err.message, stackTrace: err.stack });
+  res.json({
+    title: "Validation Failed",
+    message: err.message,
+    stackTrace: err.stack,
+  });
 };
 
 module.exports = errorHandler;
