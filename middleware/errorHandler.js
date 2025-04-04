f3348dad40a775920a00565232f9452bd6025a90
Sthembiso Radebe <stheradebe@Sthembisos-MacBook-Pro.local>
2025-04-04 08:47:58 +0200

    Changes made to controller and and middleware, and userRoutes added

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,49 +1,51 @@
-const { constants } = require("../constants");
+const constants = require("../constants");
 
 const errorHandler = (err, req, res, next) => {
   const statusCode = res.statusCode ? res.statusCode : 500;
+
   switch (statusCode) {
     case constants.VALIDATION_ERROR:
-      res.json({
+      return res.status(statusCode).json({
         title: "Validation Failed",
         message: err.message,
         stackTrace: err.stack,
       });
-      break;
+
     case constants.NOT_FOUND:
-      res.join({
+      return res.status(statusCode).json({
         title: "Not Found",
         message: err.message,
         stackTrace: err.stack,
       });
+
     case constants.UNAUTHORIZED:
-      res.join({
+      return res.status(statusCode).json({
         title: "Unauthorized",
         message: err.message,
         stackTrace: err.stack,
       });
+
     case constants.FORBIDDEN:
-      res.join({
+      return res.status(statusCode).json({
         title: "Forbidden",
         message: err.message,
         stackTrace: err.stack,
       });
+
     case constants.SERVER_ERROR:
-      res.join({
-        title: "Server error",
+      return res.status(statusCode).json({
+        title: "Server Error",
         message: err.message,
         stackTrace: err.stack,
       });
+
     default:
-      console.log("No error, all good!");
-      break;
+      return res.status(500).json({
+        title: "Unknown Error",
+        message: err.message,
+        stackTrace: err.stack,
+      });
   }
-  res.json({ title: "Not Found", message: err.message, stackTrace: err.stack });
-  res.json({
-    title: "Validation Failed",
-    message: err.message,
-    stackTrace: err.stack,
-  });
 };
 
 module.exports = errorHandler;
